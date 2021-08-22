3c3e2b243db1c7b975d0bb0e60429216d8182df9
Narges Navidi <nargesnavidi@yahoo.com>
2021-08-21 17:54:45 -0700

    initial commit-

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import About from './AboutPage'
+import Home from './HomePage'
+import Error from './ErrorPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+import AuthWrapper from './AuthWrapper'
+
+export {
+  Home,
+  About,
+  Error,
+  Cart,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+  Checkout,
+  SingleProduct,
+}
